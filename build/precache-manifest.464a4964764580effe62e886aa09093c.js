3c9f36651d2d01e272aefeeaf477e53347091c8e
rd3v <blackscreen405@yahoo.co.id>
2020-08-02 23:16:37 +0800

    add build

---
+++ build/precache-manifest.464a4964764580effe62e886aa09093c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2eba4eb3d3672b002cfa022b5d5d712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36f21b56e0a26c4bd0b6",
+    "url": "/static/css/main.e9ceb523.chunk.css"
+  },
+  {
+    "revision": "295db60ef1170aa55c97",
+    "url": "/static/js/2.624d0caa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.624d0caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f21b56e0a26c4bd0b6",
+    "url": "/static/js/main.85728e32.chunk.js"
+  },
+  {
+    "revision": "ed5afcf389165d3dab8d",
+    "url": "/static/js/runtime-main.7f26ff2a.js"
+  }
+]);
